9b1e9efc74fa9b05234351bfc02aede58fcb6ea7
HERBERT RAUSCH <hrausch@gmail.com>
2024-12-10 13:22:03 +0000

    Adicionando o cadastro de editais.
    O que mudou:
    1) lista_editais.html: neste arquivo tem a lista de todos os editais cadastro e o formulario para cadastrar.
    Veja que tem um <form> e este form é a url que processa quando o botao é assionado. ele "vchama" a view add_edital

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('adddisciplina/', views.serve_adddisciplina, name='adddisciplina'),
     path('atividades/', views.serve_atividades, name='atividades'),
     path('desempenho/', views.serve_desempenho, name='desempenho'),
+
+    path('listaedital', views.lista_editais, name='lista_editais'),
+    path('addedital1/', views.add_edital, name='add_edital'),
+    path('deleteedital/<int:id>/', views.delete_edital, name='delete_edital'),
     
     # formulários pro banco de dados
     
--- core/views.py
+++ core/views.py
@@ -3,6 +3,27 @@ from django.conf import settings
 import os
 from django.shortcuts import redirect, render
 
+from django.shortcuts import render, redirect
+from .models import Edital
+
+def lista_editais(request):
+    editais = Edital.objects.all()
+    return render(request, 'lista_editais.html', {'editais': editais})
+
+def add_edital(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        if titulo:
+            Edital.objects.create(titulo=titulo)
+    return redirect('lista_editais')
+
+def delete_edital(request, id):
+    if request.method == 'POST':
+        edital = Edital.objects.get(id=id)
+        edital.delete()
+    return redirect('lista_editais')
+
+
 # PÁGINAS
 # Função para servir a página base
 def serve_base(request):
